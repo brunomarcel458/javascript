3ed79613c6e801739dc1d9e50fe7e6cfa3b039cb
pc abrasivo <brunomarcel458@gmail.com>
2023-12-13 16:37:11 -0300

    fix function to generate dynamic ID

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,8 +1,6 @@
-var numbers = document.querySelectorAll('td');
-
-//criar ID dinâmico
-
+//create dynamic ID
 function generateId(){
+    var numbers = document.querySelectorAll('td');
     lenghtNumbers = numbers.length;
     for( n=0; n < lenghtNumbers; n++ ){
         numbers[n].id = "item_" + (n + 1);
