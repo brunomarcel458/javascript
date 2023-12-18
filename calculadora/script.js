62b4e28bbdaeff91f908b48b0400d2a81af737e6
pc abrasivo <brunomarcel458@gmail.com>
2023-12-18 16:01:01 -0300

    still in progress

--- calculadora/script.js
+++ calculadora/script.js
@@ -11,27 +11,45 @@ generateId();
 
 //add event to click on numbers
 
-var clickedNumber = document.querySelectorAll('.numbers-click td');
-clickedNumber.forEach((number)=> {
-    number.addEventListener('click', ()=> {
-        console.log( calcularSoma( parseInt(number.textContent)) );
-    })
-})
+// var clickedNumber = document.querySelectorAll('.numbers-click td');
+// clickedNumber.forEach((number)=> {
+//     number.addEventListener('click', ()=> {
+//         console.log( sumCalculate( parseInt(number.textContent)) );
+//     })
+// })
 
-//add event to click on operators
-var operator = document.querySelectorAll('.operators-click td');
-operator.forEach((op)=> {
-    op.addEventListener('click', ()=>{
-        if(op.id == 'soma'){
-            console.log(op.textContent);
-        }
-    })
-})
+// //add event to click on operators
+// var operator = document.querySelectorAll('.operators-click td');
+// operator.forEach((op)=> {
+//     op.addEventListener('click', ()=>{
+//         if(op.id == 'soma'){
+//             console.log(op.textContent);
+//         }
+//     })
+// })
 
-function calcularSoma(x) {
-    soma = x + 3;
-    // console.log(clickOnNumber())
-    return soma;
+// function sumCalculate(x) {
+//     sum = x + 3;
+//     // console.log(clickOnNumber())
+//     return sum;
+// }
+// sumCalculate();
+
+
+// function subtractionCalculate(x){
+//     subtraction = x - 3;
+//     return subtraction;
+// }
+
+// subtractionCalculate();
+
+function clickedOnNumber() {
+    var showNumberClicked = document.querySelector('.first-number');
+    var clickedNumber = document.querySelectorAll('.numbers-click td');
+    clickedNumber.forEach((number)=> {
+        number.addEventListener('click', ()=> {
+            showNumberClicked.innerHTML = showNumberClicked.innerHTML + number.textContent;
+            console.log(showNumberClicked.innerHTML);
+        })
+    })
 }
-
-calcularSoma();
