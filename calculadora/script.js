c500f267406414a3f9adace5dddd2f652db0dcf9
pc abrasivo <brunomarcel458@gmail.com>
2023-12-18 09:58:41 -0300

    add event to click on operators

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,6 +1,6 @@
 //create dynamic ID
 function generateId(){
-    var numbers = document.querySelectorAll('td');
+    var numbers = document.querySelectorAll('.numbers td');
     lenghtNumbers = numbers.length;
     for( n=0; n < lenghtNumbers; n++ ){
         numbers[n].id = "item_" + (n + 1);
@@ -8,3 +8,25 @@ function generateId(){
 }
 
 generateId();
+
+//add event to click on numbers
+function clickOnNumber(clickedNumber) {
+    clickedNumber = document.querySelectorAll('td');
+    clickedNumber.forEach((number)=> {
+        number.addEventListener('click', ()=> {
+            console.log(number.id);
+        })
+    })
+}
+
+clickOnNumber();
+
+//add event to click on operators
+function clickOnOperator(clickedOperator) {
+    clickedOperator = document.querySelectorAll('td');
+    clickedOperator.forEach((operator)=> {
+        operator.addEventListener('click', ()=> {
+            console.log(operator.id);
+        })
+    })
+};
